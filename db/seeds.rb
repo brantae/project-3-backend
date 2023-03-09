8f4713b0ffaf159e094be806347126960c3eb7d4
Anslie Brant <brantae@gmail.com>
2023-03-08 18:58:49 -0600

    updated seeds to reflect database changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,58 +5,58 @@ Festival.create([
     {
         name: "Hangout Fest",
         city: "Gulf Shores",
-        month_of_year: "May",
+        month: "May",
         website: "https://www.hangoutmusicfest.com/",
-        rank: 1
+        
     },
     {
         name: "Opelika Songwriters Festival",
         city: "Opelika",
-        month_of_year: "October",
+        month: "October",
         website: "https://www.opelikasongwritersfestival.com/",
-        rank: 8
+        
     },
     {
         name: "WhistleStop Weekend",
         city: "Gulf Shores",
-        month_of_year: "September",
+        month: "September",
         website: "https://whistlestopweekend.com/",
-        rank: 5
+        
     },
     {
         name: "Rock the South Festival",
         city: "Cullman",
-        month_of_year: "July",
+        month: "July",
         website: "http://www.rockthesouth.com/",
-        rank: 2
+        
     },
     {
         name: "Buttahatchee River Fall Fest",
         city: "Hamilton",
-        month_of_year: "October",
+        month: "October",
         website: "https://www.brff.org",
-        rank: 4
+        
     },
     {
         name: "Rakkos Revival Music & Arts Gathering",
         city: "Steele",
-        month_of_year: "May",
+        month: "May",
         website: "https://www.facebook.com/cukorakko/",
-        rank: 6
+        
     },
     {
         name: "Bob Sykes BBQ & Blues Festival",
         city: "Bessemer",
-        month_of_year: "April",
+        month: "April",
         website: "https://bobsykes.com/blues-festival/",
-        rank: 7
+        
     },
     {
         name: "Wilson Pickett Music and Arts Festival",
         city: "Prattville",
-        month_of_year: "April",
+        month: "April",
         website: "https://www.wilsonpickettfestival.com/",
-        rank: 3
+        
     },
     ])
 
